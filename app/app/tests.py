91e011cf4bf3864e58e5cf4ef1f40394800529e6
Trevor Joseph <trevor.joseph@johnlewis.co.uk>
2020-11-02 10:21:18 +0000

    fixed linting issues

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 
+from app.calc import add, subtract
 
-from app.calc import add
 
 class calcTest(TestCase):
 
-
     def test_add_numbers(self):
         """Test that 2 numbers are added together"""
-        self.assertEqual(add(3,8),11)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subrtract_numbers(self):
+        """Test that 2 numbers are subtracted"""
+        self.assertEqual(subtract(5, 5), 0)
